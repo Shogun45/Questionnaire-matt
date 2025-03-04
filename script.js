32b804aa023af994e9bc76389882094f99aad9ac
Shogun45 <maciejakmatthieu@gmail.com>
2025-03-04 20:14:56 +0100

    update

--- script.js
+++ script.js
@@ -11,20 +11,20 @@ const questions = [
     new Question("Combien de groupe d'amis j'ai", "3", 2),
     new Question("Comment s'appelle ma Lilie ?", "Alexis", 2),
     new Question("En qui je crois ?", "Moi", 1),
-    new Question("Combien de nuances de Matthieu ?", 40, 1)
+    new Question("Combien de nuances de Matthieu ?", "40", 1)
 ];
 
 let currentIndex = 0;
 
-let startBox = document.getElementById("start-box");
-let startButton = document.getElementById("start-button");
+let startBox = window.getElementById("start-box");
+let startButton = window.getElementById("start-button");
 
-let questionBox = document.getElementById("question-box");
-let questionText = document.getElementById("question-text");
-let answerInput = document.getElementById("answer-input");
-let feedBackText = document.getElementById("feedback-text");
+let questionBox = window.getElementById("question-box");
+let questionText = window.getElementById("question-text");
+let answerInput = window.getElementById("answer-input");
+let feedBackText = window.getElementById("feedback-text");
 
-let victoryBox = document.getElementById("victory-box");
+let victoryBox = window.getElementById("victory-box");
 
 function displayQuestion() {
     if (currentIndex < questions.length) {
