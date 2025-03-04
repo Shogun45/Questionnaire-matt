a7a35d58ef078dbd6ab94f20c6b3655fd09a539b
Shogun45 <maciejakmatthieu@gmail.com>
2025-03-04 21:26:36 +0100

    update

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ class Question {
     constructor(question, answer, difficulty) {
         this.question = question;
         this.answer = answer.toLowerCase();
-        this.difficulty = difficulty;
     }
 }
 
 const questions = [
-    new Question("Quel est ma date de naissance ?", "17 juillet 2000", 1),
-    new Question("Combien de groupe d'amis j'ai", "3", 2),
-    new Question("Comment s'appelle ma Lilie ?", "Alexis", 2),
-    new Question("En qui je crois ?", "Moi", 1),
-    new Question("Combien de nuances de Matthieu ?", "40", 1)
+    new Question("Quel est ma date de naissance ?", "17 juillet 2000"),
+    new Question("Combien de groupe d'amis j'ai", "3"),
+    new Question("Comment s'appelle ma Lilie ?", "Alexis"),
+    new Question("En qui je crois ?", "Moi"),
+    new Question("Combien de nuances de Matthieu ?", "40")
 ];
 
 let currentIndex = 0;
@@ -20,11 +19,6 @@ let startBox = document.getElementById("start-box");
 let startButton = document.getElementById("start-button");
 
 let questionBox = document.getElementById("question-box");
-let questionText = document.getElementById("question-text");
-let answerInput = document.getElementById("answer-input");
-let feedBackText = document.getElementById("feedback-text");
-
-let victoryBox = document.getElementById("victory-box");
 
 function displayQuestion() {
     if (currentIndex < questions.length) {
