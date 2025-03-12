244c88b2914004dceaa1e6d259d01ec988bff8ad
Shogun45 <maciejakmatthieu@gmail.com>
2025-03-12 21:32:59 +0100

    update

--- sw.js
+++ sw.js
@@ -1,15 +1,29 @@
+const CACHE_NAME = "v1.0.3"
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+];
+
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open("v1").then((cache) => {
-            return cache.addAll(["/", "./index.html", "./styles.css", "./script.js", "./manifest.json"]);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
         }).catch((err) => console.error("Erreur de mise en cache :", err))
     );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        caches.match(event.request).then(response => {
+            // Si la ressource est dans le cache, on la renvoie
+            if (response) {
+                return response;
+            }
+
+            // Sinon on essaie de la chercher en ligne
+            return fetch(event.request).catch(() => caches.match('/offline.html'));
         })
     );
-});
+})
